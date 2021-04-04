935f34b99b26bad1c5ab44e34113936cdedf2ad4
leotor <leonid.toropov@wartsila.com>
2021-04-04 16:28:23 +0300

    welcome

--- bot.py
+++ bot.py
@@ -28,6 +28,24 @@ async def test(ctx, *args):
 
 
 @bot.event
+async def on_member_join(member):
+    welcome = bot.get_channel(channels.WELCOME)
+    text = f"""
+Добро пожаловать в Таверну Сказаний!
+
+{member.mention}, для доступа к каналам ознакомься с {bot.get_channel(channels.RULES).mention} и поставь под ними ✅.
+
+-Информация о гильдии Tavern of Tales - {bot.get_channel(channels.INFO).mention}
+-Заявка для вступления в гильдию (заполняется одним сообщением прямо на канале) - {bot.get_channel(channels.JOIN).mention}
+-Выбрать себе классовую роль для доступа к соответствующему каналу - {bot.get_channel(channels.CHOOSE_CLASS).mention}
+-Гостевая для общения - {bot.get_channel(channels.GUEST).mention}
+-На сервере доступна система приватных комнат - {bot.get_channel(channels.PRIVATE_CHANNELS).mention}
+{bot.get_emoji(828026991361261619)}
+"""
+    await welcome.send(text)
+
+
+@bot.event
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     emoji: discord.Emoji = payload.emoji
     guild: discord.Guild = bot.get_guild(payload.guild_id)
