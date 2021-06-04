ac13b3833dca1036f831d85ff924ae1f211c17ec
leotor <leonid.toropov@wartsila.com>
2021-06-05 01:08:04 +0300

    animated reactions

--- commands/misc.py
+++ commands/misc.py
@@ -1,3 +1,5 @@
+from discord.ext import commands
+
 from database import reconnect
 from database.stat import select_all
 from init_bot import bot
@@ -8,6 +10,7 @@ async def test_select(ctx, *args):
     await ctx.send(select_all())
 
 
+@commands.has_role("Глава ги")
 @bot.command(pass_context=True, help='Переконектиться к базе данных')
 async def connect(ctx, *args):
     reconnect()
--- events/messages.py
+++ events/messages.py
@@ -6,7 +6,7 @@ from discord.utils import get
 from commands import automoderation, send_by_bot
 from constants import channels
 from init_bot import bot
-from utils.guild_utils import check_for_beer, find_animated_emoji
+from utils.guild_utils import check_for_beer, find_animated_emoji, get_renference_author
 
 
 class MessageHandler:
@@ -35,27 +35,39 @@ class MessageHandler:
             await inv_gi_channel.send(f"<@{self.message.author.id}>", embed=embed)
             await self.message.delete()
 
-    async def replace_animated_emoji(self) -> bool:
-        animated_emoji_flag = False
+    async def replace_animated_emoji(self) -> list:
+        animated_emojis_ids = []
 
         content = self.message.content
         words = set(content.split(':'))
         for word in words:
             emoji = find_animated_emoji(word)
             if emoji and f':{word}:' in content:  # only 1 word without ::
-                animated_emoji_flag = True
+                animated_emojis_ids.append(emoji.strip(">").split(':')[-1])
                 content = content.replace(f':{word}:', emoji)
         self.message._handle_content(content)
 
-        return animated_emoji_flag
+        return animated_emojis_ids
 
     async def send_message(self, is_animated: bool):
+        ctx = await bot.get_context(self.message)
+
         if is_animated:
             if self.message.author.bot:
                 return
-            ctx = await bot.get_context(self.message)
+            # todo переделать
+            # reference_author = await get_renference_author(ctx)
+            # await send_by_bot(ctx, f"{reference_author.mention if reference_author else ''}\n{self.message.content}", delete=True)
             await send_by_bot(ctx, self.message.content, delete=True)
         await bot.process_commands(self.message)
+
+    async def add_reactions(self, animated_emojis_ids):
+        ctx = await bot.get_context(self.message)
+
+        message_id = ctx.message.reference.message_id
+        message = await ctx.fetch_message(message_id)
+        for emoji_id in animated_emojis_ids:
+            await message.add_reaction(await ctx.guild.fetch_emoji(emoji_id))
 
 
 @bot.event
@@ -64,10 +76,13 @@ async def on_message(message: discord.Message):
 
     check_for_beer(message.content)
 
-    is_animated = await handler.replace_animated_emoji()
+    animated_emojis_ids = await handler.replace_animated_emoji()
+    is_animated = animated_emojis_ids != []
 
     await handler.swear_moderation()
     await handler.on_mems_channel()
     await handler.on_join_to_guild_channel()
 
     await handler.send_message(is_animated)
+    if message.reference:
+        await handler.add_reactions(animated_emojis_ids)
