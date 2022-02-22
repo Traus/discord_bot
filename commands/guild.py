c8ea582458ec964f24bfd7fb7a295709c219b85d
leotor <leonid.toropov@wartsila.com>
2022-02-23 02:17:31 +0300

    decayra

--- commands/guild.py
+++ commands/guild.py
@@ -1,3 +1,4 @@
+import re
 from random import randint
 
 import discord
@@ -5,7 +6,7 @@ from discord.ext import commands
 from discord.utils import get
 
 from commands.mute_control import _add_mute
-from constants import roles
+from constants import roles, channels
 from init_bot import bot
 from utils.format import box, send_by_bot
 from utils.guild_utils import is_spam, get_members_by_role, strip_tot, is_traus
@@ -36,6 +37,40 @@ class GuildCommands(commands.Cog, name='Гильдия'):
                 msg += box("Общий сбор!")
 
             await send_by_bot(ctx, msg, delete=True)
+
+    @commands.command(pass_context=True, name='тлевра',  help='Тлевра и всё, что с ней связано.')
+    @commands.has_any_role("Совет ги", "Актив гильдии", "Наставник")
+    async def decayra(self, ctx, *time):
+        channel: discord.TextChannel = ctx.channel
+        decayra_channel = get(ctx.guild.channels, id=channels.DECAYRA)
+        if channel != decayra_channel:
+            return
+
+        all_roles = ctx.guild.roles
+        councils = get(all_roles, id=roles.COUNCILS)
+        tot = get(all_roles, id=roles.TOT)
+        recruit = get(all_roles, id=roles.RECRUIT)
+        pings = f'{councils.mention} {tot.mention} {recruit.mention}'
+        pattern = f"Кто идёт, ставим ✅ под сообщением!\n" \
+                  f"Группы собираем в пещерах, но не перед входом в 3.1. Бежим тихо."
+        msg = f"Тлевра в {' '.join(time)}. {pattern}\n" \
+              f"Тактику всем знать наизусть: <#{channels.GUIDES}>"
+
+        if time:
+            new_message: discord.Message = await send_by_bot(ctx, f"{pings}\n{msg}", delete=True)
+            await new_message.add_reaction('✅')
+            for message in await channel.pins():
+                if pattern in message.content:
+                    await message.unpin()
+            await new_message.pin()
+        else:
+            await ctx.message.delete()
+            found = ''
+            for message in await channel.pins():
+                if pattern in message.content:
+                    found = ''.join(re.findall('Тлевра в .*(?=\.)', message.content))
+                    break
+            await ctx.send(box(found))
 
     @commands.command(pass_context=True, name='роль', help="Список членов ги с определенной ролью")
     @commands.has_any_role("Совет ги", "ToT")
