64d1fac74b46f1a16738505617c8e6cba0e1c291
leotor <leonid.toropov@wartsila.com>
2022-12-04 16:01:30 +0300

    DATABASE_URL_ELEPHANT_SQL

--- database/__init__.py
+++ database/__init__.py
@@ -2,14 +2,14 @@ import os
 import psycopg2
 
 try:
-    from local_settings import DATABASE_URL
+    from local_settings import DATABASE_URL_ELEPHANT_SQL
 except ImportError:
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL_ELEPHANT_SQL = os.environ.get("DATABASE_URL_ELEPHANT_SQL")
 
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL_ELEPHANT_SQL, sslmode='require')
 
 
 def reconnect():
     global conn
-    return psycopg2.connect(DATABASE_URL, sslmode='require')
+    return psycopg2.connect(DATABASE_URL_ELEPHANT_SQL, sslmode='require')
