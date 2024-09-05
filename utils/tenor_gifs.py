d18961377f27d9187ae5a620fcb7045b2bffa22a
Traus <leonid.toropov@gmail.com>
2024-09-05 16:29:23 +0300

    временное говно

--- utils/tenor_gifs.py
+++ utils/tenor_gifs.py
@@ -11,9 +11,10 @@ except ImportError:
 
 
 def find_gif(search_term, limit=10):
-    r = requests.get(
-        "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, TENOR_API, limit))
-
-    result = json.loads(r.content)
-    gif_url = result['results'][randint(0, limit-1)]['media'][0]['gif']['url']
-    return gif_url
+    raise Exception('Траус дебил, не починил бота и свалил в туман')
+    # r = requests.get(
+    #     "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, TENOR_API, limit))
+    #
+    # result = json.loads(r.content)
+    # gif_url = result['results'][randint(0, limit-1)]['media'][0]['gif']['url']
+    # return gif_url
